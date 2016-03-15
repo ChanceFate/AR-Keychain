b015c36ed6384cc54bed0d25a9dc3d409dcc29a1
Nick Borchers <nick.borchers@gmai.com>
2016-03-15 06:14:13 -0400

    Initial Commit

---
+++ Assets/Scripts/OpenPresent.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenPresent : MonoBehaviour
+{
+
+    public GameObject prefab;
+    public Collider coll;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        coll = GetComponent<Collider>();
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+#if UNITY_STANDALONE
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(coll.Raycast(ray,out hit,100F))
+            {
+                OnMouseDown();
+            }
+        }
+
+#elif UNITY_ANDROID
+
+        if(Input.touchCount == 1)
+        {
+           if(Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hit;
+
+                if (coll.Raycast(ray, out hit, 100F))
+                {
+                    OnMouseDown();
+                }
+            }
+        }
+
+#endif
+
+    }
+
+    void OnMouseDown()
+    {
+        GameObject obj = Instantiate(prefab, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+        obj.transform.parent = this.transform.parent;
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/SwitchImage.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchImage : MonoBehaviour
+{
+
+    public Material[] materials;
+    public Collider coll;
+    public Renderer rend;
+
+    private int index = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        coll = GetComponent<Collider>();
+
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (materials.Length == 0)
+            return;
+
+#if UNITY_STANDALONE
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(coll.Raycast(ray,out hit,100F))
+            {
+                OnMouseDown();
+            }
+        }
+
+#elif UNITY_ANDROID
+
+        if (Input.touchCount == 1)
+        {
+            if (Input.GetTouch(0).phase == TouchPhase.Began)
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+                RaycastHit hit;
+
+                if (coll.Raycast(ray, out hit, 100F))
+                {
+                    OnMouseDown();
+                }
+            }
+        }
+
+#endif
+
+    }
+
+    void OnMouseDown()
+    {
+        index++;        
+        int matnum = (index/2) % (materials.Length);
+
+        Debug.Log(matnum);
+
+        rend.sharedMaterial = materials[matnum];
+    }
+}

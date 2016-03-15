cfaa852217b604504d59699575e30aa290e958c5
Nick Borchers <nick.borchers@gtri.gatech.edu>
2016-03-15 12:47:21 -0400

    PlayMovie script now works for unity standalone player.

--- Assets/Scripts/PlayMovie.cs
+++ Assets/Scripts/PlayMovie.cs
@@ -4,12 +4,43 @@ using System.Collections;
 public class PlayMovie : MonoBehaviour {
 
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
+
+
 	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
-	}
+	void Update ()
+    {
+
+        Renderer r = GetComponent<Renderer>();
+        MovieTexture movie = (MovieTexture)r.material.mainTexture;
+        movie.loop = true;
+
+        bool paused = true;
+
+#if UNITY_STANDALONE
+
+        // If the space bar is pressed
+        if (Input.GetButtonDown ("Jump"))
+        {
+
+            if(movie.isPlaying)
+            {
+                movie.Pause();
+                paused = true;
+            }
+            else
+            {
+                movie.Play();
+                paused = false;
+            }
+
+        }
+
+#endif
+
+    }
 }

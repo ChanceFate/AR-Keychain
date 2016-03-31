0d7659387adce45afb664e368312467012b0c00b
Nick Borchers <nick.borchers@gmai.com>
2016-03-30 20:56:31 -0400

    Added editor script to build asset bundles, and built movietexture AssetBundle.

---
+++ Assets/Editor/ExportAssetBundle.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class ExportAssetBundle
+{
+
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        BuildPipeline.BuildAssetBundles("Assets/AssetBundles");
+    }
+}
